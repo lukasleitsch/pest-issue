28c8c607653d80d810db3600a87f62a3b41ff94c
Lukas Leitsch <lukas@leitsch.org>
2024-09-11 05:31:57 +0200

    add comments

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,8 +1,8 @@
 <?php
 
 dataset('dataset A', [
-    [1, 1, 1],
-    ['parameterA' => 1, 'parameterB' => 1, 'parameterC' => 1]
+    [1, 1, 1], // works in Pest 2 and Pest 3
+    ['parameterA' => 1, 'parameterB' => 1, 'parameterC' => 1] // works in Pest 2 and fails in Pest 3
 ]);
 
 test('dataset with same parameters count', function ($parameterA, $parameterB, $parameterC) {
